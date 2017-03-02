25b20304447c9aa4ee07288105183956cf1741b7
Francesco Visin <fvisin@gmail.com>
2017-03-02 16:58:13 +0100

    Put hash in __version__

--- keras/__init__.py
+++ keras/__init__.py
@@ -15,4 +15,7 @@ from . import objectives
 from . import optimizers
 from . import regularizers
 
-__version__ = '1.2.2'
+from subprocess import check_output
+
+__version__ = check_output('git rev-parse HEAD',
+                           shell=True).strip().decode('ascii')
